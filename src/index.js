26e22169c04ef19ab4d2fca5919725a5335bd549
Leonel <leoneleduardomadridotmail.com>
2020-11-18 14:58:19 -0700

    bot running

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const {Client , MessageAttachment} = require('discord.js');
+const client = new Client();
+const {prefix, meaning_of_life} = require('./config.json'); 
 require('dotenv').config();
 
-const guildID = 778253660928868382;
-const SpamChannelID = 778673993829187604;
+const guildID = '778253660928868382';
+const SpamChannelID = '778673993829187604';
+
+const respuestas = [
+	'hola',
+	'hola cachora XD',
+	'pene',
+	'banana',
+	'caca',
+	'culo'
+];
 
 client.once('ready', () =>{
 	console.log('im ready man');
@@ -11,8 +21,26 @@ client.once('ready', () =>{
 
 client.login(process.env.BOT_TOKEN);
 
-client.on('message', (msg) => {
+client.on('message', async (msg) => {
 	if (msg.guild.id === guildID && msg.channel.id === SpamChannelID) {
-		console.log(msg);
+		if(msg.author.id !== '778667957596848160'){
+			if(msg.content.toLowerCase() === 'puta'){
+				await msg.channel.send('tu madre');
+			}
+			if(msg.content.toLowerCase() === `${prefix}user-info`){
+				const userAvatar = new MessageAttachment(msg.author.avatarURL);
+				await msg.reply(`\nyour username: ${msg.author.username}\nYour ID: ${msg.author.id}\n${userAvatar}`);
+			}
+			if(msg.content.toLowerCase() === 'cual es el sentido de la vida?'){
+				await msg.reply(`${meaning_of_life}`);
+			}
+			if(msg.content.toLowerCase() === 'hola'){
+				const r = Math.floor(Math.random() * respuestas.length);
+				msg.channel.send(respuestas[r]);
+			}
+			if (msg.content.toLowerCase() === `${prefix}commands` || msg.content.toLowerCase() === `${prefix}comandos`){
+				await msg.channel.send('visita el canal de #bot-commands para sbaer mis comandos');
+			}
+		}
 	}
 });
